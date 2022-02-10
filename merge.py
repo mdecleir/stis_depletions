d3ccdfa4fe9d7c6396020f5b3e21075093bf760d
mdecleir <mdecleir@stsci.edu>
2022-02-10 18:07:49 -0500

    Fixed how bad data are handled when resampling

--- merge.py
+++ merge.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from astropy.io import fits
 from astropy.table import Table
-from matplotlib import pyplot as plt
 from scipy.interpolate import *
 
 
@@ -45,23 +44,29 @@ def merge_orders(filename):
     new_waves = np.arange(min_wave, max_wave, new_delta, dtype="float64")
 
     # place all orders (len(data)) on this wavelength grid, using nearest neighbors
-    # do this for the fluxes, uncertainties, data quality flags and net count rates (i.e. 4 arrays)
-    # only use the good data
-    new_data = np.full((len(data), 4, len(new_waves)), np.nan)
+    # do this for the fluxes, uncertainties, and net count rates (i.e. 3 arrays)
+    # note that this will not extrapolate values; outside of the wavelength range of a certain order, values will be NaNs for that order
+    # create a new data frame
+    new_data = np.full((len(data), 3, len(new_waves)), np.nan)
     for j in range(len(data)):
+        # only use the good data in the resampling; masking out the bad data before resampling results in an interpolation over the bad regions, which we don't want; replace the bad data with NaNs before resampling in order to avoid interpolation over the bad regions; this will result in "gaps" (with NaNs) in the resampled spectrum where the data is bad
         good = (data["DQ"][j] & sdqflags) == 0
+        data["FLUX"][j][~good] = np.nan
+        data["ERROR"][j][~good] = np.nan
+        data["NET"][j][~good] = np.nan
+        # resample the fluxes, uncertainties and net count rates
         func = interpolate.interp1d(
-            data["WAVELENGTH"][j][good],
+            data["WAVELENGTH"][j],
             [
-                data["FLUX"][j][good],
-                data["ERROR"][j][good],
-                data["DQ"][j][good],
-                data["NET"][j][good],
+                data["FLUX"][j],
+                data["ERROR"][j],
+                data["NET"][j],
             ],
             kind="nearest",
             bounds_error=False,
         )
         new_data[j] = func(new_waves)
+
     # save the new data as an Astropy Table
     new_data_table = Table(new_data, names=["FLUX", "ERROR", "DQ", "NET"])
 
